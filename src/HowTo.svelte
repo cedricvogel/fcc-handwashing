9348e0e4ace89f4dfaf6b39375fcddddac5160c4
Cédric Vogel <cedric.vogel@me.com>
2021-01-12 23:55:12 +0100

    commit HTML

---
+++ src/HowTo.svelte
@@ -0,0 +1,12 @@
+<style>
+    img {
+        max-width: 100%;
+    }
+</style>
+
+<div bp="grid">
+    <img
+        src="handwashing.gif"
+        alt="How to Handwash"
+        bp="offset-5@md 4@md 12@sm" />
+</div>
